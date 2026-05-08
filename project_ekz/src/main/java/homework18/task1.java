7b655448c21e5076350b030854619459d1f389d6
UlyanaNazarova <142579592+ekz-xoxo@users.noreply.github.com>
2026-05-08 09:58:12 +0700

    add task1
    таблица истинности

--- project_ekz/src/main/java/homework18/task1.java
+++ project_ekz/src/main/java/homework18/task1.java
@@ -1,4 +1,19 @@
 package main.java.homework18;
 
 public class task1 {
+    public  static void main(String[] args){
+        System.out.println("P\t\tQ\t\tP AND Q\tP OR Q\tP XOR Q\tNOT P");
+        boolean p = false;
+        boolean q = false;
+        System.out.println(p + "\t" + q + "\t" + (p && q) + "\t" + (p || q) + "\t" + (p ^ q) + "\t" + (!p));
+        p = true;
+        q = false;
+        System.out.println(p + "\t" + q + "\t" + (p && q) + "\t" + (p || q) + "\t" + (p ^ q) + "\t" + (!p));
+        p = false;
+        q = true;
+        System.out.println(p + "\t" + q + "\t" + (p && q) + "\t" + (p || q) + "\t" + (p ^ q) + "\t" + (!p));
+        p = true;
+        q = true;
+        System.out.println(p + "\t" + q + "\t" + (p && q) + "\t" + (p || q) + "\t" + (p ^ q) + "\t" + (!p));
+    }
 }
